37ab52bc484f6a57717348b11f82264de5a17cf2
PHALNAJA <nathaphol_008@windowslive.com>
2017-10-12 13:59:07 +0700

    Add rpt.go in CTRL

---
+++ ctrl/rpt.go
@@ -0,0 +1,34 @@
+package ctrl
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "192.168.0.163"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "backup"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
+
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package do_api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/liverbee/do_api/ctrl"
+	"gopkg.in/gin-contrib/cors.v1"
+	//"github.com/gin-gonic/contrib/cors"
+)
+
+func main(){
+	r := gin.New()
+
+	r.Use(cors.Default())
+	r.Run(":9000")
+
+}
